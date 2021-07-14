29380163cb1b1939cfb5ad6b9aba8f2410d41433
qasim-wpbrigade <87133962+qasim-wpbrigade@users.noreply.github.com>
2021-07-14 17:15:02 +0500

    update
    update

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ $(document).ready(function () {
     dots: true,
     prevArrow: ".hprev-arrow",
     nextArrow: ".hnext-arrow",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
   });
   $(".product-slider").slick({
     speed: 300,
@@ -17,7 +28,31 @@ $(document).ready(function () {
     dots: true,
     prevArrow: "<span class='slide-arrow prev-arrow'><img src='./img/hero-slider-leftt-arrow.png' alt=''></span>",
     nextArrow: "<span class='slide-arrow next-arrow'><img src='./img/hero-slider-right-arrow.png' alt=''></span>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $("a.play-btn").fancybox();
 });
+
+function closenotification(){
+  document.getElementById('notiBar').style.display = 'none';
+}
